83ce5564a5db7fcfa8d99051b59f87d92a71180c
VaishaliRakshe <vaishalijadhav000@gmail.com>
2021-11-15 10:51:52 -0600

    update

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,16 +15,30 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
+//   countByType(type: string): number {
+// 	let count = 0;
+// 	if (this.satellites) {
+// 	  for (let i = 0; i < this.satellites.length; i++) {
+// 		 if (this.satellites[i].type === type) {
+// 			count++;
+// 		 }
+// 	  }
+// 	}
+// 	return count;
+//  }
+
+
+countByType(type: string): number {
+	let numArr = [];
+	
 	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
+		 if (this.satellites[i].type.toLocaleLowerCase() === type) {
+			 numArr.push(this.satellites[i].name);
+			
 		 }
 	  }
-	}
-	return count;
+	
+	return numArr.length;
  }
 
 
